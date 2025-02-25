aaecdeff4029003ca8cad9fc05f794612d35cf80
John Borkowski <johnbork18@gmail.com>
2025-02-25 11:47:25 -0600

    Fixed main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
 #include <string>
-
-struct School {
-    std::string name;
-    std::string address;
-    std::string city;
-    std::string state;
-    std::string county;
-    School* next;
-
-    School(std::string n, std::string address, std::string c, std::string s, std::string co)
-        : name(n), address(address), city(c), state(s), county(co), next(nullptr) {}
-};
-
-
-
-
+#include <iostream>
+#include <string>
 
 int main() {
+
+
     return 0;
 }
