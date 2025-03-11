c7f7e1f9439c31821e24a2fa58dab66aa528c378
John Borkowski <johnbork18@gmail.com>
2025-03-11 11:19:10 -0500

    add SchoolBST class

---
+++ SchoolBST.cpp
@@ -0,0 +1,5 @@
+//
+// Created by johnb on 3/11/2025.
+//
+
+#include "SchoolBST.h"
---
+++ SchoolBST.h
@@ -0,0 +1,16 @@
+//
+// Created by johnb on 3/11/2025.
+//
+
+#ifndef SCHOOLBST_H
+#define SCHOOLBST_H
+
+
+
+class SchoolBST {
+
+};
+
+
+
+#endif //SCHOOLBST_H
