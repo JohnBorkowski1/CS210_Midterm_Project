d480e7f6f0bde444c03422a32287fd8b71c3ff5d
John Borkowski <johnbork18@gmail.com>
2025-03-11 12:02:25 -0500

    finished SchoolBST.h

--- SchoolBST.h
+++ SchoolBST.h
@@ -56,7 +56,55 @@ private:
         cout << root->data.name << "\n";
     }
 
+    Node* minValueNode(Node* node) {
+        while (node && node->left)
+            node = node->left;
+        return node;
+    }
 
+    Node* deleteByName(Node* root, string name) {
+        if (!root) return root;
+        if (name < root->data.name)
+            root->left = deleteByName(root->left, name);
+        else if (name > root->data.name)
+            root->right = deleteByName(root->right, name);
+        else {
+            if (!root->left) {
+                Node* temp = root->right;
+                delete root;
+                return temp;
+            } else if (!root->right) {
+                Node* temp = root->left;
+                delete root;
+                return temp;
+            }
+            Node* temp = minValueNode(root->right);
+            root->data = temp->data;
+            root->right = deleteByName(root->right, temp->data.name);
+        }
+        return root;
+    }
+
+    Node* findByName(Node* root, string name) {
+        if (!root || root->data.name == name) return root;
+        if (name < root->data.name) return findByName(root->left, name);
+        return findByName(root->right, name);
+    }
+
+public:
+    SchoolBST() : root(nullptr) {}
+
+    void insert(School school) { root = insert(root, school); }
+    void displayInOrder() { displayInOrder(root); }
+    void displayPreOrder() { displayPreOrder(root); }
+    void displayPostOrder() { displayPostOrder(root); }
+
+    School* findByName(string name) {
+        Node* result = findByName(root, name);
+        return result ? &result->data : nullptr;
+    }
+
+    void deleteByName(string name) { root = deleteByName(root, name); }
 };
 
 
